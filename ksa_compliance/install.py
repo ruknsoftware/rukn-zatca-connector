a8205cc39c1ea9c58bb2e96b1556e40a90b8263f
HAMZA ABUABADA <abadahhamza@gmail.com>
2025-06-23 22:09:34 +0300

    feat(Payment Entry): remove unnecessary custom fields

--- ksa_compliance/install.py
+++ ksa_compliance/install.py
@@ -15,32 +15,7 @@ def add_custom_fields():
 				fieldtype="Check",
 				insert_after="payment_type",
 				module=ksa_compliance_module
-			),
-			dict(
-				fieldname="zatca_advance_payment_column_break1",
-				fieldtype="Column Break",
-				insert_after="paid_amount",
-				module=ksa_compliance_module
-			),
-			dict(
-				fieldname="net_total",
-				label="Advance Payment Total",
-				fieldtype="Currency",
-				insert_after="zatca_advance_payment_column_break1",
-				depends_on="eval:doc.is_advance_payment == 1",
-				read_only=1,
-				module=ksa_compliance_module
-			),
-			dict(
-				fieldname="tax_amount",
-				label="Advance Payment Tax Amount",
-				fieldtype="Currency",
-				insert_after="net_total",
-				depends_on="eval:doc.is_advance_payment == 1",
-				read_only=1,
-				module=ksa_compliance_module
-			),
-
+			)
 		],
 	}
 	create_custom_fields(custom_fields)
