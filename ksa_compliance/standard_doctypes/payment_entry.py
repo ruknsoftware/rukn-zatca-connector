2f4b7a62f5054dc36561130f4e39394111722411
HAMZA ABUABADA <abadahhamza@gmail.com>
2025-06-23 22:03:48 +0300

    feat(Advance Payment): read taxes from payemnt entry then set gl depends on and advance_payment_account

--- ksa_compliance/standard_doctypes/payment_entry.py
+++ ksa_compliance/standard_doctypes/payment_entry.py
@@ -9,84 +9,68 @@ from ksa_compliance.ksa_compliance.doctype.zatca_business_settings.zatca_busines
 
 class AdvancePaymentEntry(PaymentEntry):
 
+    def check_is_advance_payment_entry(self) -> bool:
+        return self.is_advance_payment and self.payment_type == "Receive" and self.party_type == "Customer"
+
     def add_tax_gl_entries(self, gl_entries):
-        if self.is_advance_payment:
+        if self.check_is_advance_payment_entry():
             self.add_advance_payment_tax_gl_entries(gl_entries)
         else:
             super().add_tax_gl_entries(gl_entries)
 
     def add_advance_payment_tax_gl_entries(self, gl_entries):
-        taxes = get_taxes_and_charges_details(self)
-        taxes["cost_center"] = self.cost_center
-        taxes["tax_amount"] = taxes["base_tax_amount"] = self.tax_amount
-        taxes["total"] = self.net_total
-        account_currency = get_account_currency(taxes.account_head)
-        if account_currency != self.company_currency:
-            frappe.throw(
-                _("Currency for {0} must be {1}").format(taxes.account_head, self.company_currency))
+        for d in self.get("taxes"):
+            account_currency = get_account_currency(d.account_head)
+            if account_currency != self.company_currency:
+                frappe.throw(_("Currency for {0} must be {1}").format(d.account_head, self.company_currency))
 
-        if self.payment_type in ("Pay", "Internal Transfer"):
-            dr_or_cr = "debit" if taxes.add_deduct_tax == "Add" else "credit"
+            dr_or_cr = "credit" if d.add_deduct_tax == "Add" else "debit"
             rev_dr_or_cr = "credit" if dr_or_cr == "debit" else "debit"
-            against = self.party or self.paid_from
-        elif self.payment_type == "Receive":
-            dr_or_cr = "credit" if taxes.add_deduct_tax == "Add" else "debit"
-            rev_dr_or_cr = "credit" if dr_or_cr == "debit" else "debit"
+
             against = self.party or self.paid_to
 
-        tax_amount = taxes.tax_amount
-        base_tax_amount = taxes.base_tax_amount
+            tax_amount = d.tax_amount
+            base_tax_amount = d.base_tax_amount
 
-        gl_entries.append(
-            self.get_gl_dict(
-                {
-                    "account": taxes.account_head,
-                    "against": against,
-                    dr_or_cr: tax_amount,
-                    dr_or_cr + "_in_account_currency": base_tax_amount
-                    if account_currency == self.company_currency
-                    else taxes.tax_amount,
-                    "cost_center": taxes.cost_center,
-                    "post_net_value": True,
-                },
-                account_currency,
-                item=taxes,
+            gl_entries.append(
+                self.get_gl_dict(
+                    {
+                        "account": d.account_head,
+                        "against": against,
+                        dr_or_cr: tax_amount,
+                        dr_or_cr + "_in_account_currency": base_tax_amount
+                        if account_currency == self.company_currency
+                        else d.tax_amount,
+                        "cost_center": d.cost_center,
+                        "post_net_value": True,
+                    },
+                    account_currency,
+                    item=d,
+                )
             )
-        )
-        tax_gl_entry = gl_entries[0]
-        settings = ZATCABusinessSettings.for_company(self.company)
-        advance_payment_account = settings.advance_payment_account
-        if get_account_currency(advance_payment_account) != self.company_currency:
-            if self.payment_type == "Receive":
-                exchange_rate = self.target_exchange_rate
-            elif self.payment_type in ["Pay", "Internal Transfer"]:
-                exchange_rate = self.source_exchange_rate
-            base_tax_amount = flt((tax_amount / exchange_rate), self.precision("paid_amount"))
+            settings = ZATCABusinessSettings.for_company(self.company)
+            advance_payment_account = settings.advance_payment_account
+            if advance_payment_account:
+                if get_account_currency(advance_payment_account) != self.company_currency:
+                    exchange_rate = self.target_exchange_rate
+                    base_tax_amount = flt((tax_amount / exchange_rate), self.precision("paid_amount"))
 
-        gl_entries.append(
-            self.get_gl_dict(
-                {
-                    "account": advance_payment_account,
-                    "against": against,
-                    rev_dr_or_cr: tax_amount,
-                    rev_dr_or_cr + "_in_account_currency": base_tax_amount
-                    if account_currency == self.company_currency
-                    else tax_gl_entry.tax_amount,
-                    "cost_center": self.cost_center,
-                    "post_net_value": True,
-                },
-                account_currency,
-                item=tax_gl_entry,
-            )
-        )
-
-
-def set_advance_payment_amounts(doc, method):
-    if not doc.is_advance_payment:
-        return
-    tax_rate = get_taxes_and_charges_details(doc).get("rate")
-    doc.net_total = round(doc.base_paid_amount / (1 + (tax_rate / 100)))
-    doc.tax_amount = round(doc.base_paid_amount - doc.net_total)
+                gl_entries.append(
+                    self.get_gl_dict(
+                        {
+                            "account": advance_payment_account,
+                            "against": against,
+                            rev_dr_or_cr: tax_amount,
+                            rev_dr_or_cr + "_in_account_currency": base_tax_amount
+                            if account_currency == self.company_currency
+                            else d.tax_amount,
+                            "cost_center": self.cost_center,
+                            "post_net_value": True,
+                        },
+                        account_currency,
+                        item=d,
+                    )
+                )
 
 
 def get_company_default_taxes_and_charges_template(payment_entry):
