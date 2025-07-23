ec7bc296334b8d77a535b8808fbb63b4cf63caab
HAMZA ABUABADA <abadahhamza@gmail.com>
2025-07-23 14:48:40 +0300

    feat(Sales Invoice): set advance payments on advances child table

--- ksa_compliance/public/js/sales_invoice.js
+++ ksa_compliance/public/js/sales_invoice.js
@@ -18,7 +18,30 @@ frappe.require("/assets/ksa_compliance/js/update_invoice_mode_of_payment.js").th
         },
         refresh:function (frm){
             update_sales_invoice_mode_of_payment(frm);
-        }
+        },
+        customer: function(frm) {
+            if (frm.doc.customer) {
+                frappe.call({
+                    method: "ksa_compliance.standard_doctypes.sales_invoice_advance.set_advance_payments",
+                    args: { self: frm.doc,},
+                    callback: function(response) {
+                        frm.clear_table("advances");
+                        (response.message || []).forEach(advance => {
+                            let advance_row = frappe.model.add_child(frm.doc, "Sales Invoice Advance", "advances");
+                            advance_row.doctype = advance.doctype
+                            advance_row.reference_type = advance.reference_type
+                            advance_row.reference_name = advance.reference_name
+                            advance_row.reference_row = advance.reference_row
+                            advance_row.remarks = advance.remarks
+                            advance_row.advance_amount = advance.advance_amount
+                            advance_row.allocated_amount = advance.allocated_amount
+                            advance_row.ref_exchange_rate = advance.ref_exchange_rate
+                        });
+                        frm.refresh_field("advances");
+                    }
+                });
+            }
+        },
     })
 
     frappe.ui.form.on('Sales Invoice Item', {
