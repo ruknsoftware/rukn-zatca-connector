e6803fd70195594daf0a62795269ec888a4bf290
HAMZA ABUABADA <abadahhamza@gmail.com>
2025-07-30 14:19:17 +0300

    fix(Auto Advance payment): get party account (#22)
    * fix(Auto Advance payment): get party account
    
    * feat(Advance Payment Invoice): move check advance invoice to new utils file to avoid circular import
    
    * feat(Advance Payment Invoice): avoid adding advance payment on advance invoice
    
    * feat: update version
    
    * feat(Advance Payment Invoice): raise exception on adding payments on advance invoice

--- ksa_compliance/standard_doctypes/sales_invoice_advance.py
+++ ksa_compliance/standard_doctypes/sales_invoice_advance.py
@@ -1,12 +1,14 @@
 import json
-
+from typing import cast
 import frappe
 from frappe.utils import flt
 from frappe import qb
 from frappe.query_builder.custom import ConstantColumn
 from erpnext.accounts.doctype.sales_invoice.sales_invoice import SalesInvoice
 from erpnext.accounts.doctype.pos_invoice.pos_invoice import POSInvoice
+from erpnext.accounts.party import get_party_account
 from ksa_compliance.ksa_compliance.doctype.zatca_business_settings.zatca_business_settings import ZATCABusinessSettings
+from ksa_compliance.utils.advance_payment_invoice import is_advance_payment_invoice
 
 
 def get_invoice_advance_payments(self: SalesInvoice | POSInvoice):
@@ -103,9 +105,15 @@ def get_prepayment_info(self: SalesInvoice | POSInvoice):
 @frappe.whitelist()
 def get_invoice_applicable_advance_payments(self):
     self = json.loads(self)
-    settings = ZATCABusinessSettings.for_company(self.get("company"))
+    self = cast(SalesInvoice, frappe.get_doc(self))
+    company = self.get("company")
+    settings = ZATCABusinessSettings.for_company(company)
     if not settings.auto_apply_advance_payments:
         return []
+    if is_advance_payment_invoice(self, settings):
+        return []
+    customer = self.get("customer")
+    party_account = get_party_account(party_type="Customer", party=customer, company=company)
     payment_entry = qb.DocType("Payment Entry")
     advance_payment_entries = (
         qb.from_(payment_entry)
@@ -119,9 +127,9 @@ def get_invoice_applicable_advance_payments(self):
             payment_entry.paid_from_account_currency.as_("currency"),
         )
         .where(
-            (payment_entry.paid_from == self.get("debit_to"))
+            (payment_entry.paid_from == party_account)
             & (payment_entry.party_type == "Customer")
-            & (payment_entry.party == self.get("customer"))
+            & (payment_entry.party == customer)
             & (payment_entry.payment_type == "Receive")
             & (payment_entry.docstatus == 1)
             & (payment_entry.unallocated_amount.gt(0))
