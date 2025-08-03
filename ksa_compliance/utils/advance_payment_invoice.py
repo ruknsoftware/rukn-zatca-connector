52f6c1c7b2ed515f6ff92548028d13d9c515731d
HAMZA ABUABADA <abadahhamza@gmail.com>
2025-08-03 13:12:56 +0300

    fix(Sales invoice): validate set advances on advance invoice (#23)
    * fix(Sales invoice): validate set advances on advance invoice
    
    * feat(Auto Advance Paymnet): fetch advance payments on validate
    
    * feat(Auto Advance Paymnet): check having applicable advance payments
    
    * feat(Advance Payment): rename check valid advance invoice
    
    * feat(Advance Payment): check auto applay advance payments before applicable advance
    
    * feat(Advance Payment): rename check invoice has advance item

--- ksa_compliance/utils/advance_payment_invoice.py
+++ ksa_compliance/utils/advance_payment_invoice.py
@@ -2,6 +2,6 @@ from erpnext.accounts.doctype.sales_invoice.sales_invoice import SalesInvoice
 from ksa_compliance.ksa_compliance.doctype.zatca_business_settings.zatca_business_settings import ZATCABusinessSettings
 
 
-def is_advance_payment_invoice(self: SalesInvoice, settings: ZATCABusinessSettings) -> bool:
+def invoice_has_advance_item(self: SalesInvoice, settings: ZATCABusinessSettings) -> bool:
     items = [item.item_code for item in self.items]
     return settings.advance_payment_item in items
