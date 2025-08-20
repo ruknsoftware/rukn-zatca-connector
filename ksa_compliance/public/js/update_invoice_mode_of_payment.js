5784dfccf68b161006b5792075052a6095b9fd80
HAMZA ABUABADA <abadahhamza@gmail.com>
2025-08-20 12:36:17 +0300

    feat: Set Update Outstanding for Self Read Only On Return Advance Payment Invoice (#44)
    * feat: split fuction check advance invoice and update mode of payment field
    
    * feat: Set Update Outstanding for Self Read Only On Return Advance Payment Invoice
    
    * feat: update version and changelog

--- ksa_compliance/public/js/update_invoice_mode_of_payment.js
+++ ksa_compliance/public/js/update_invoice_mode_of_payment.js
@@ -1,18 +1,17 @@
-
-function update_sales_invoice_mode_of_payment(frm) {
-    frappe.db.get_value(
+function is_advance_invoice(frm) {
+    return frappe.db.get_value(
         "ZATCA Business Settings",
         { company: frm.doc.company },
         "advance_payment_item"
     ).then(response => {
         const advance_item = response.message.advance_payment_item;
-        const is_advance = (frm.doc.items || [])
-            .some(row => row.item_code === advance_item);
+        return (frm.doc.items || []).some(row => row.item_code === advance_item);
+    });
+}
 
-        frm.set_df_property("mode_of_payment",
-            "read_only",
-            is_advance ? 0 : 1
-        );
+function update_sales_invoice_mode_of_payment(frm) {
+    is_advance_invoice(frm).then(is_advance => {
+        frm.set_df_property("mode_of_payment", "read_only", is_advance ? 0 : 1);
         frm.toggle_reqd("mode_of_payment", is_advance);
         frm.refresh_field("mode_of_payment");
     });
