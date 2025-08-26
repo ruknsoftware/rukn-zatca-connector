fac428b4bb7b4cfe6b6e69bbec59c732cf7dc379
Abdul-AMA <abdamapro22@gmail.com>
2025-08-26 17:11:05 +0300

    feat: map each default mode of payment to its correct ZATCA code

--- ksa_compliance/utils/mode_of_payment.py
+++ ksa_compliance/utils/mode_of_payment.py
@@ -1,8 +1,15 @@
 import frappe
 
 def set_zatca_code_on_default_mop(doc, method):
+    payment_code_map = {
+        "Cash": "10",          
+        "Cheque": "20",      
+        "Credit Card": "54",  
+        "Wire Transfer": "30", #Credit transfer
+        "Bank Draft": "21"     
+    }
 
-    default_modes = ["Cash", "Cheque", "Credit Card", "Wire Transfer", "Bank Draft"]
-    
-    if doc.mode_of_payment in default_modes and not doc.custom_zatca_payment_means_code:
-        doc.custom_zatca_payment_means_code = "4461"
+    if not doc.custom_zatca_payment_means_code:
+        zatca_code = payment_code_map.get(doc.mode_of_payment)
+        if zatca_code:
+            doc.custom_zatca_payment_means_code = zatca_code
