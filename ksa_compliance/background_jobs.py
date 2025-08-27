4fe2f7a6ecdae16c799389a66e79d0eb76b1ed47
HAMZA ABUABADA <abadahhamza@gmail.com>
2025-08-27 15:01:21 +0300

    feat: background_jobs apply pre-commit

--- ksa_compliance/background_jobs.py
+++ ksa_compliance/background_jobs.py
@@ -16,27 +16,29 @@ from ksa_compliance.ksa_compliance.doctype.sales_invoice_additional_fields.sales
 @frappe.whitelist()
 def add_batch_to_background_queue(check_date=datetime.date.today()):
     try:
-        logger.info('Start Enqueue E-Invoices')
+        logger.info("Start Enqueue E-Invoices")
         frappe.enqueue(
-            'ksa_compliance.background_jobs.sync_e_invoices',
+            "ksa_compliance.background_jobs.sync_e_invoices",
             check_date=check_date,
-            queue='long',
+            queue="long",
             timeout=3480,  # 58 minutes, so that we can run it hourly
-            job_name='Sync E-Invoices',
+            job_name="Sync E-Invoices",
             deduplicate=True,
-            job_id=f'Sending invoices {check_date}',
+            job_id=f"Sending invoices {check_date}",
         )
     except Exception as ex:
-        logger.error('An error occurred queueing the job', exc_info=ex)
+        logger.error("An error occurred queueing the job", exc_info=ex)
 
 
 def sync_e_invoices(
-    check_date: Optional[datetime.datetime | datetime.date] = None, batch_size: int = 100, dry_run: bool = False
+    check_date: Optional[datetime.datetime | datetime.date] = None,
+    batch_size: int = 100,
+    dry_run: bool = False,
 ):
-    prefix = '[Dry run] ' if dry_run else ''
-    logger.info(f'{prefix}Syncing with ZATCA in batches of {batch_size}')
+    prefix = "[Dry run] " if dry_run else ""
+    logger.info(f"{prefix}Syncing with ZATCA in batches of {batch_size}")
     if check_date:
-        logger.info(f'{prefix}Limiting sync to >= date: {check_date}')
+        logger.info(f"{prefix}Limiting sync to >= date: {check_date}")
 
     # We can't use a numerical offset and increment it by the number of records because of the nature of the query.
     # We're querying for draft sales invoice additional fields then submitting them. Let's say we start with offset 0
@@ -49,7 +51,9 @@ def sync_e_invoices(
     # The solution is to use the creation date itself as an offset/filter. We sort by it ascending, so after every
     # batch we can query for fields whose creation > the last creation in the previous batch
     if isinstance(check_date, datetime.date):
-        offset = cast(Optional[datetime.datetime], datetime.datetime.combine(check_date, datetime.time.min))
+        offset = cast(
+            Optional[datetime.datetime], datetime.datetime.combine(check_date, datetime.time.min)
+        )
     else:
         offset = cast(Optional[datetime.datetime], check_date)
 
@@ -59,32 +63,33 @@ def sync_e_invoices(
         if not additional_field_docs:
             break
 
-        logger.info(f'{prefix}Syncing {len(additional_field_docs)} after date/time {offset}')
+        logger.info(f"{prefix}Syncing {len(additional_field_docs)} after date/time {offset}")
         offset = additional_field_docs[-1].creation
 
         for doc in additional_field_docs:
             try:
-                logger.info(f'{prefix}Submitting {doc.name}')
+                logger.info(f"{prefix}Submitting {doc.name}")
                 if dry_run:
                     continue
 
                 adf_doc = cast(
-                    SalesInvoiceAdditionalFields, frappe.get_doc('Sales Invoice Additional Fields', doc.name)
+                    SalesInvoiceAdditionalFields,
+                    frappe.get_doc("Sales Invoice Additional Fields", doc.name),
                 )
                 result = adf_doc.submit_to_zatca()
                 message = result.ok_value if is_ok(result) else result.err_value
-                logger.info(f'{prefix}{doc.name}: {message}')
+                logger.info(f"{prefix}{doc.name}: {message}")
                 frappe.db.commit()
             except Exception:
-                logger.error(f'{prefix}Error submitting {doc.name}', exc_info=True)
+                logger.error(f"{prefix}Error submitting {doc.name}", exc_info=True)
                 frappe.db.rollback()
 
-    logger.info(f'{prefix}Sync Done')
+    logger.info(f"{prefix}Sync Done")
 
 
 def build_query(check_date: Optional[datetime.datetime], limit: int) -> QueryBuilder:
-    batch_status = ['Ready For Batch', 'Resend', 'Corrected']
-    doctype = DocType('Sales Invoice Additional Fields')
+    batch_status = ["Ready For Batch", "Resend", "Corrected"]
+    doctype = DocType("Sales Invoice Additional Fields")
     query = (
         frappe.qb.from_(doctype)
         .select(doctype.name, doctype.creation)
