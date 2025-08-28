52a363612aadcc7bc17b3e0ad64e69c22729bc15
Abdul-AMA <abdamapro22@gmail.com>
2025-08-28 14:59:22 +0300

    fix: pre-commit

--- ksa_compliance/install.py
+++ ksa_compliance/install.py
@@ -3,98 +3,107 @@ from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
 
 ksa_compliance_module = "KSA Compliance"
 
+
 def after_install():
-	add_custom_fields()
-	add_property_setters()
+    add_custom_fields()
+    add_property_setters()
 
 
 def add_custom_fields():
-	custom_fields = {
-		"Payment Entry": [
-			dict(
-				fieldname="is_advance_payment",
-				label="IS Advance Payment",
-				fieldtype="Check",
-				insert_after="payment_type",
-				module=ksa_compliance_module,
-				read_only = True,
-			),
-			dict(
-				fieldname="invoice_doctype",
-				label="Invoice Doctype",
-				fieldtype="Select",
-				options="Sales Invoice",
-				insert_after="mode_of_payment",
-				module=ksa_compliance_module,
-				hidden=True,
-				read_only=True,
-			),
-			dict(
-				fieldname="advance_payment_invoice",
-				label="Advance Payment Invoice",
-				fieldtype="Dynamic Link",
-				options="invoice_doctype",
-				insert_after="invoice_doctype",
-				module=ksa_compliance_module,
-				read_only=True,
-			)
-		],
-		"Sales Invoice": [
-			dict(
-				fieldname="mode_of_payment",
-				label="Mode Of Payment",
-				fieldtype="Link",
-				insert_after="company_tax_id",
-				options="Mode of Payment",
-				module=ksa_compliance_module,
-				read_only=True,
-			),
-			dict(
-				fieldname="advance_payment_invoices",
-				label="Advance Payment Invoices",
-				fieldtype="Table",
-				insert_after="advances",
-				options="Sales Invoice Advance Payment",
-				module=ksa_compliance_module,
-				read_only=True,
-			),
-		],
+    custom_fields = {
+        "Payment Entry": [
+            dict(
+                fieldname="is_advance_payment",
+                label="IS Advance Payment",
+                fieldtype="Check",
+                insert_after="payment_type",
+                module=ksa_compliance_module,
+                read_only=True,
+            ),
+            dict(
+                fieldname="invoice_doctype",
+                label="Invoice Doctype",
+                fieldtype="Select",
+                options="Sales Invoice",
+                insert_after="mode_of_payment",
+                module=ksa_compliance_module,
+                hidden=True,
+                read_only=True,
+            ),
+            dict(
+                fieldname="advance_payment_invoice",
+                label="Advance Payment Invoice",
+                fieldtype="Dynamic Link",
+                options="invoice_doctype",
+                insert_after="invoice_doctype",
+                module=ksa_compliance_module,
+                read_only=True,
+            ),
+        ],
+        "Sales Invoice": [
+            dict(
+                fieldname="mode_of_payment",
+                label="Mode Of Payment",
+                fieldtype="Link",
+                insert_after="company_tax_id",
+                options="Mode of Payment",
+                module=ksa_compliance_module,
+                read_only=True,
+            ),
+            dict(
+                fieldname="advance_payment_invoices",
+                label="Advance Payment Invoices",
+                fieldtype="Table",
+                insert_after="advances",
+                options="Sales Invoice Advance Payment",
+                module=ksa_compliance_module,
+                read_only=True,
+            ),
+        ],
         "Mode of Payment": [
             dict(
                 fieldname="custom_zatca_payment_means_code",
                 label="ZATCA Payment Means Code",
                 fieldtype="Data",
                 insert_after="accounts",
-				dt="Mode of Payment",
+                dt="Mode of Payment",
                 reqd=0,
                 description="Value from <a href='https://unece.org/fileadmin/DAM/trade/untdid/d16b/tred/tred4461.htm' target='_blank'>UN/EDIFACT 4461</a>",
-                module=ksa_compliance_module
+                module=ksa_compliance_module,
             )
-        ]
-	}
-	create_custom_fields(custom_fields)
+        ],
+    }
+    create_custom_fields(custom_fields)
+
 
 def add_property_setters():
-    frappe.make_property_setter({
-        "doctype_or_field": "DocType",
-        "doctype": "Mode of Payment",
-        "property": "field_order",
-        "value": '["mode_of_payment", "enabled", "type", "accounts", "custom_zatca_payment_means_code"]',
-    },module=ksa_compliance_module)
+    frappe.make_property_setter(
+        {
+            "doctype_or_field": "DocType",
+            "doctype": "Mode of Payment",
+            "property": "field_order",
+            "value": '["mode_of_payment", "enabled", "type", "accounts", "custom_zatca_payment_means_code"]',
+        },
+        module=ksa_compliance_module,
+    )
+
 
 def after_migrate():
 
     doctype = "Mode of Payment"
     fieldname = "custom_zatca_payment_means_code"
-    
+
     setter_name = f"{doctype}-{fieldname}-reqd"
 
     if not frappe.db.exists("Property Setter", setter_name):
-        
-        frappe.make_property_setter({
-            "doctype": doctype,
-            "fieldname": fieldname,
-            "property": "reqd", 
-            "value": "1",
-            "property_type": "Check"
-        },module=ksa_compliance_module)
+
+        frappe.make_property_setter(
+            {
+                "doctype": doctype,
+                "fieldname": fieldname,
+                "property": "reqd",
+                "value": "1",
+                "property_type": "Check",
+            },
+            module=ksa_compliance_module,
+        )
