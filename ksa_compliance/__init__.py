f43b55b0e4c0bc8f7716f58d2d23898c1fb934aa
Alaa Alsalehi <alaadiaa@gmail.com>
2025-09-16 22:11:20 +0300

    Fix: logger issue on version 14 (#94)
    * Fix: logger issue on version 14
    
    * Fix: style
    
    * Fix: style

--- ksa_compliance/__init__.py
+++ ksa_compliance/__init__.py
@@ -1,9 +1,20 @@
 import logging
+import os
 
 from frappe.utils.logger import get_logger
 
-logger = get_logger("zatca", max_size=1_000_000)
-logger.setLevel(logging.INFO)
+# Initialize logger with error handling for missing directories
+try:
+    logger = get_logger("zatca", max_size=1_000_000)
+    logger.setLevel(logging.INFO)
+except (FileNotFoundError, OSError):
+    logger = logging.getLogger("zatca")
+    logger.setLevel(logging.INFO)
+    if not logger.handlers:
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter("%(asctime)s %(levelname)s zatca %(message)s")
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
 
 
-__version__ = "0.52.4"
+__version__ = "0.52.5"
