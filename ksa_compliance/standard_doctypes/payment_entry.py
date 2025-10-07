185ef95eedf862920d0261ff3c269f2944f5fd29
HAMZA ABUABADA <abadahhamza@gmail.com>
2025-10-07 16:13:54 +0300

    feat: add_tax_gl_entries for advance payment entry

--- ksa_compliance/standard_doctypes/payment_entry.py
+++ ksa_compliance/standard_doctypes/payment_entry.py
@@ -3,8 +3,9 @@ from erpnext.accounts.general_ledger import (
     make_gl_entries,
     process_gl_map,
 )
-from erpnext.controllers.accounts_controller import get_taxes_and_charges
+from erpnext.accounts.utils import get_account_currency
 from frappe import _
+from frappe.utils import flt
 
 from ksa_compliance.ksa_compliance.doctype.zatca_business_settings.zatca_business_settings import (
     ZATCABusinessSettings,
@@ -49,10 +50,9 @@ def get_company_default_taxes_and_charges_template(payment_entry):
     )
 
 
-def get_taxes_and_charges_details(payment_entry):
+def get_taxes_and_charges(payment_entry):
     item_tax_template = get_company_default_taxes_and_charges_template(payment_entry)
-    taxes = get_taxes_and_charges("Sales Taxes and Charges Template", item_tax_template)[0]
-    return taxes
+    return frappe.get_doc("Sales Taxes and Charges Template", item_tax_template)
 
 
 def set_advance_payment_entry_settling_references(payment_entry):
@@ -85,3 +85,72 @@ def set_advance_payment_entry_settling_references(payment_entry):
     advance_payment_entry_doc.unallocated_amount -= payment_entry.paid_amount
     advance_payment_entry_doc.flags.ignore_validate_update_after_submit = True
     advance_payment_entry_doc.save()
+
+
+def add_tax_gl_entries(doc, method):
+    settings = ZATCABusinessSettings.for_company(doc.company)
+    if (
+        not settings
+        or settings.advance_payment_depends_on != "Payment Entry"
+        or not doc.is_advance_payment_entry
+    ):
+        return
+    tax = get_taxes_and_charges(doc).taxes[0]
+    gl_entries = []
+
+    account_currency = get_account_currency(tax.get("account_head"))
+    if account_currency != doc.company_currency:
+        frappe.throw(
+            _("Currency for {0} must be {1}").format(tax.get("account_head"), doc.company_currency)
+        )
+
+    if doc.payment_type in ("Pay", "Internal Transfer"):
+        dr_or_cr = "debit" if tax.get("add_deduct_tax") == "Add" else "credit"
+        rev_dr_or_cr = "credit" if dr_or_cr == "debit" else "debit"
+        against = doc.party or doc.paid_from
+    elif doc.payment_type == "Receive":
+        dr_or_cr = "credit" if tax.get("add_deduct_tax") == "Add" else "debit"
+        rev_dr_or_cr = "credit" if dr_or_cr == "debit" else "debit"
+        against = doc.party or doc.paid_to
+
+    tax_rate = tax.rate
+    amount = flt(doc.paid_amount)
+    net_amount = round(amount / (1 + (tax_rate / 100)), 2)
+    tax_amount = round(flt(amount - net_amount), 2)
+
+    gl_entries.append(
+        doc.get_gl_dict(
+            {
+                "account": tax.get("account_head"),
+                "against": against,
+                dr_or_cr: tax_amount,
+                dr_or_cr
+                + "_in_account_currency": (
+                    tax_amount
+                    if account_currency == doc.company_currency
+                    else tax.get("tax_amount")
+                ),
+                "cost_center": tax.get("cost_center"),
+                "post_net_value": True,
+            },
+            account_currency,
+            item=tax,
+        )
+    )
+
+    gl_entries.append(
+        doc.get_gl_dict(
+            {
+                "account": settings.advance_payment_tax_account,
+                "against": tax.get("account_head"),
+                rev_dr_or_cr: tax_amount,
+                rev_dr_or_cr + "_in_account_currency": tax_amount,
+                "cost_center": tax.get("cost_center"),
+                "post_net_value": True,
+            },
+            account_currency,
+            item=tax,
+        )
+    )
+    gl_entries = process_gl_map(gl_entries)
+    make_gl_entries(gl_entries)
