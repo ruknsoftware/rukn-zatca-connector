6bf8e69ab1335a5040feef61419fcfab8f8171d6
HAMZA ABUABADA <abadahhamza@gmail.com>
2025-10-07 16:11:10 +0300

    feat: check advance_payment_depends_on on check advance payment invoices

--- ksa_compliance/utils/advance_payment_invoice.py
+++ ksa_compliance/utils/advance_payment_invoice.py
@@ -9,10 +9,14 @@ from ksa_compliance.ksa_compliance.doctype.zatca_business_settings.zatca_busines
 @frappe.whitelist()
 def get_advance_payment_item(company):
     return frappe.get_value(
-        "ZATCA Business Settings", {"company": company}, "advance_payment_item"
+        "ZATCA Business Settings",
+        {"company": company, "advance_payment_depends_on": "Sales Invoice"},
+        "advance_payment_item",
     )
 
 
 def invoice_has_advance_item(self: SalesInvoice, settings: ZATCABusinessSettings) -> bool:
+    if settings.advance_payment_depends_on != "Sales Invoice":
+        return False
     items = [item.item_code for item in self.items]
     return settings.advance_payment_item in items
